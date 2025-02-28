a613e3c41638bc6ad257149606bbdb1867e23827
Teodora Udrea <teodora.udrea1998@gmail.com>
2025-02-28 22:22:05 +0000

    added astro ovidius theme integrated with tinaCMS

--- public/burger.js
+++
@@ -1,9 +0,0 @@
-// public/burger.js
-const burger = document.querySelector('.burger');
-const internalLinks = document.querySelector('.internal-links');
-
-if (burger && internalLinks) {
-  burger.addEventListener('click', () => {
-    internalLinks.classList.toggle('active');
-  });
-}
--- src/consts.ts
+++
@@ -1,5 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Dr Cătălin-Gabriel Stănescu';
-export const SITE_DESCRIPTION = 'Dr Cătălin-Gabriel Stănescu\'s blogofolio';
