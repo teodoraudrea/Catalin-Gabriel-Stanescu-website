55b42d576f57809376b6602d806aa2e631dae7aa
Teodora Udrea <teodora.udrea1998@gmail.com>
2026-01-30 23:50:28 +0000

    chore(tina): add generated Tina artifacts and built admin (for schema reindex)

---
+++ tina/__generated__/client.ts
@@ -0,0 +1,5 @@
+import { createClient } from "tinacms/dist/client";
+import { queries } from "./types";
+export const client = createClient({ cacheDir: '/Users/teo/code-github/Catalin-Gabriel-Stanescu-website/tina/__generated__/.cache/1769816867839', url: 'https://content.tinajs.io/1.6/content/2d1137a0-60c0-48b1-8255-6ce1571aba55/github/main', token: 'fa2e52aafca779efc74ef72a069cf5d584becf39', queries,  });
+export default client;
+  
